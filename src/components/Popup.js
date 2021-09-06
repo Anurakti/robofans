155794245a0832d2c13b2b76c611835a7b17d0c4
Anurakti <anuraktiarora92@gmail.com>
2021-09-06 19:13:39 +0530

    adding PopUp display

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './popup.css'
+import RoboImage from "./images/Robot_image.jpg"
 
 function Popup(props) {
     return (
         <div className="popup--back">
             <div className="popup--content">
                 <div className="popup--close" onClick={props.closePopup}>x</div>
-
+                    <h1 className="tc">Robots Online</h1>
+                    <img src={RoboImage} alt="robot"/>
             </div>
         </div>
     )
